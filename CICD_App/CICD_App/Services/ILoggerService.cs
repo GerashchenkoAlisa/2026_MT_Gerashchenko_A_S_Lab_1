46bea00d298d5a9f0a3d4775f4f1b67b91731bfa
Lisanna <alisa020306@gmail.com>
2026-03-28 19:48:59 +0200

    add logger service

---
+++ CICD_App/CICD_App/Services/ILoggerService.cs
@@ -0,0 +1,9 @@
+﻿namespace CICD_App.Services;
+
+public interface ILoggerService
+{
+    void LogInfo(string message);
+    void LogSuccess(string message);
+    void LogError(string message);
+    void LogWarning(string message);
+}
