b854a9c23bed8e45985576d962ecc3d1ad7c566c
Lisanna <alisa020306@gmail.com>
2026-06-15 02:43:21 +0300

    push some entities 2

---
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Entities/MessageSeverity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_2.Entities;
+
+[Table("MessageSeverities")]
+public class MessageSeverity : BaseEntity<int>
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int MessageSeverityId { get; set; }
+
+    public override int Id => this.MessageSeverityId;
+
+    [Required]
+    [MaxLength(20)]
+    public string SeverityName { get; set; } = string.Empty;
+
+    [MaxLength(200)]
+    public string? SeverityDescription { get; set; }
+
+    public virtual ICollection<BuildMessage> BuildMessages { get; } = [];
+
+    public override string ToLogString(string val = "")
+        => base.ToLogString($"{this.SeverityName} {val}".TrimEnd());
+}
