b9f812e0d2e28b6456d2eb5f45844a08176fa25f
Lisanna <alisa020306@gmail.com>
2026-06-15 01:22:24 +0300

    push some entities

---
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Entities/BuildExecution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_2.Entities;
+
+[Table("BuildExecutions")]
+public class BuildExecution : BaseEntity<int>
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int BuildExecutionId { get; set; }
+
+    public override int Id => this.BuildExecutionId;
+
+    [Required]
+    public int ApplicationId { get; set; }
+
+    [Required]
+    public int ProcessStageId { get; set; }
+
+    [Required]
+    public int ExecutionResultId { get; set; }
+
+    [Required]
+    public DateTime ExecutionStartTime { get; set; }
+
+    [Required]
+    public long ExecutionTimeMs { get; set; }
+
+    [Required]
+    public int ExitCode { get; set; }
+
+    [NotMapped]
+    public int ErrorCount => this.BuildMessages?.Count(m => m.MessageSeverity?.SeverityName == "Error") ?? 0;
+
+    [NotMapped]
+    public int WarningCount => this.BuildMessages?.Count(m => m.MessageSeverity?.SeverityName == "Warning") ?? 0;
+
+    [ForeignKey(nameof(ApplicationId))]
+    public virtual Application Application { get; set; } = null!;
+
+    [ForeignKey(nameof(ProcessStageId))]
+    public virtual ProcessStage ProcessStage { get; set; } = null!;
+
+    [ForeignKey(nameof(ExecutionResultId))]
+    public virtual ExecutionResult ExecutionResult { get; set; } = null!;
+
+    public virtual ICollection<BuildMessage> BuildMessages { get; } = [];
+
+    public override string ToLogString(string val = "")
+        => base.ToLogString($"[{this.ExecutionResult?.ResultName}] App={this.ApplicationId} Stage={this.ProcessStageId} {val}".TrimEnd());
+}
