d14b2eb84166aeba065696a14bf706336b7ed91f
Lisanna <alisa020306@gmail.com>
2026-06-15 09:07:05 +0300

    push some entities 3

---
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Entities/ServerConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_2.Entities;
+
+[Table("ServerConfigurations")]
+public class ServerConfiguration : BaseEntity<int>
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ServerConfigurationId { get; set; }
+
+    public override int Id => this.ServerConfigurationId;
+
+    public int? ProcessorModelId { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(5,2)")]
+    public decimal MemoryCapacityGb { get; set; }
+
+    [Required]
+    public int SystemEnvironmentId { get; set; }
+
+    [ForeignKey(nameof(ProcessorModelId))]
+    public virtual ProcessorModel? ProcessorModel { get; set; }
+
+    [ForeignKey(nameof(SystemEnvironmentId))]
+    public virtual SystemEnvironment SystemEnvironment { get; set; } = null!;
+
+    public virtual ICollection<PerformanceMetric> PerformanceMetrics { get; } = [];
+
+    public override string ToLogString(string val = "")
+        => base.ToLogString(
+            $"{this.SystemEnvironment?.EnvironmentName} RAM={this.MemoryCapacityGb}GB {val}".TrimEnd());
+}
