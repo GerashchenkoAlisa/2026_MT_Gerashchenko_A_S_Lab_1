20f9ae0b056397b1d54aa6daa727354950c3bc52
Lisanna <alisa020306@gmail.com>
2026-06-16 21:31:51 +0300

    push some repo

---
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Repository/BenchmarkTestRepository.cs
@@ -0,0 +1,16 @@
+﻿using _2026_MT_Gerashchenko_A_S_Lab_2.Data;
+using _2026_MT_Gerashchenko_A_S_Lab_2.Entities;
+using _2026_MT_Gerashchenko_A_S_Lab_2.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_2.Repository;
+
+public class BenchmarkTestRepository(BuildSystemDbContext context)
+    : BaseDataRepository<BenchmarkTest>(context), IBenchmarkTestRepository
+{
+    public async Task<BenchmarkTest?> GetByTestDescriptionAsync(string testDescription)
+    {
+        return await this.DbSet.FirstOrDefaultAsync(bt => bt.TestDescription == testDescription).ConfigureAwait(false);
+    }
+}
---
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Repository/IApplicationRepository.cs
@@ -0,0 +1,12 @@
+﻿using _2026_MT_Gerashchenko_A_S_Lab_2.Data;
+using _2026_MT_Gerashchenko_A_S_Lab_2.Entities;
+using System.Threading.Tasks;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_2.Repository;
+
+public interface IApplicationRepository : IDataRepository<Application>
+{
+    Task<Application?> GetByRepositoryPathAsync(string repositoryPath);
+
+    Task<Application?> GetWithBuildHistoryAsync(int id);
+}
