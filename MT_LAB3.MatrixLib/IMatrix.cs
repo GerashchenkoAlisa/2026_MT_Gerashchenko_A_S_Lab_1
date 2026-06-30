b7cdfa54c500e8c249b9a8459138efd28ff826a8
Lisanna <alisa020306@gmail.com>
2026-06-30 23:54:40 +0300

    push Interface

---
+++ MT_LAB3.MatrixLib/IMatrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT_LAB3.MatrixLib
+{
+    internal class IMatrix
+    {
+    }
+}
