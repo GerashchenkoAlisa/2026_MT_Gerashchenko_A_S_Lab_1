cd73fc7cdc382fcf7840d793cd76f34d3b341fd8
Lisanna <alisa020306@gmail.com>
2026-06-30 18:10:39 +0300

    Dummy commit

--- 2026_MT_Gerashchenko_A_S_Lab_1/obj/Debug/net8.0/2026_MT_Gerashchenko_A_S_Lab_1.AssemblyInfo.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_1/obj/Debug/net8.0/2026_MT_Gerashchenko_A_S_Lab_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2026_MT_Gerashchenko_A_S_Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c028fd1162a5547731f9cf683f4172deb13d4a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4eca3f1228a3f6018d45ac09f86787c9f7f0ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("2026_MT_Gerashchenko_A_S_Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2026_MT_Gerashchenko_A_S_Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
