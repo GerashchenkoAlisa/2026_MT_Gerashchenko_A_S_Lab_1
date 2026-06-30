acb129d28d515b36e4b9b5bb9078659145491c24
Lisanna <alisa020306@gmail.com>
2026-06-30 23:29:01 +0300

    create PerfomanceTest

--- MT_LAB3.PerformanceTest/Program.cs
+++ MT_LAB3.PerformanceTest/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace MT_LAB3.PerformanceTest
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace MT_LAB3.PerformanceTest
+{
+    internal class Program
+    {
     }
 }
