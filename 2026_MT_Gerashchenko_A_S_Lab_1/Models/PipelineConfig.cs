9d94bc859d466eb8af59748e0d0a522c2331ac41
Lisanna <alisa020306@gmail.com>
2026-07-01 20:43:22 +0300

    delete lab1

--- 2026_MT_Gerashchenko_A_S_Lab_1/Models/PipelineConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-public class PipelineConfig
-{
-    [JsonPropertyName("pipeline")]
-    public List<PipelineStage>? Pipeline { get; set; }
-}
--- 2026_MT_Gerashchenko_A_S_Lab_1/Services/ICommandExecutor.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface ICommandExecutor
-{
-    Task<int> ExecuteCommandAsync(string command, string arguments, string workingDirectory);
-}
