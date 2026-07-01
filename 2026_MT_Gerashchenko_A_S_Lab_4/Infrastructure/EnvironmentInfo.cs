89a5656c07f508388a217f63775cfa53d62372e5
Lisanna <alisa020306@gmail.com>
2026-07-01 20:57:47 +0300

    create Infrastructure

---
+++ 2026_MT_Gerashchenko_A_S_Lab_4/Infrastructure/EnvironmentInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2026_MT_Gerashchenko_A_S_Lab_4.Infrastructure
+{
+    internal class EnvironmentInfo
+    {
+    }
+}
