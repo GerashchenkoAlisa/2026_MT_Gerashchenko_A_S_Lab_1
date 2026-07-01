ee6b614d88383f27211100f1a584ce81d7834e6f
Lisanna <alisa020306@gmail.com>
2026-07-02 00:05:00 +0300

    push some models

--- 2026_MT_Gerashchenko_A_S_Lab_4/Models/DownloadResult.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_4/Models/DownloadResult.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2026_MT_Gerashchenko_A_S_Lab_4.Models
+﻿namespace _2026_MT_Gerashchenko_A_S_Lab_4.Models;
+public record DownloadResult
 {
-    internal class DownloadResult
+    public DownloadResult(Uri url, bool isSuccess, long bytesWritten, string? error = null)
     {
+        Url = url;
+        IsSuccess = isSuccess;
+        BytesWritten = bytesWritten;
+        Error = error;
     }
+
+    public Uri Url { get; }
+    public bool IsSuccess { get; }
+    public long BytesWritten { get; }
+    public string? Error { get; }
 }
--- 2026_MT_Gerashchenko_A_S_Lab_4/Models/ScanResult.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_4/Models/ScanResult.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2026_MT_Gerashchenko_A_S_Lab_4.Models
+﻿namespace _2026_MT_Gerashchenko_A_S_Lab_4.Models;
+public record ScanResult
 {
-    internal class ScanResult
+    public ScanResult(Uri url, int? statusCode, long contentLength, TimeSpan responseTime, string? error = null)
     {
+        Url = url;
+        StatusCode = statusCode;
+        ContentLength = contentLength;
+        ResponseTime = responseTime;
+        Error = error;
     }
+
+    public Uri Url { get; }
+    public int? StatusCode { get; }
+    public long ContentLength { get; }
+    public TimeSpan ResponseTime { get; }
+    public string? Error { get; }
+
+    public bool IsSuccess => Error is null && StatusCode is >= 200 and < 300;
 }
