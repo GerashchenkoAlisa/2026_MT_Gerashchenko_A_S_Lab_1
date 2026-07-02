10eb45cdeeceaebb4bdacf74ee28773677c40f06
Lisanna <alisa020306@gmail.com>
2026-07-03 01:12:02 +0300

    push all

--- 2026_MT_Gerashchenko_A_S_Lab_4/Services/HttpProcessor.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_4/Services/HttpProcessor.cs
@@ -1,8 +1,6 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using _2026_MT_Gerashchenko_A_S_Lab_4.Infrastructure;
 using _2026_MT_Gerashchenko_A_S_Lab_4.Models;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace _2026_MT_Gerashchenko_A_S_Lab_4.Services;
 public sealed class HttpProcessor(IHttpClientFactory httpClientFactory) : IHttpProcessor
@@ -88,7 +86,7 @@ public sealed class HttpProcessor(IHttpClientFactory httpClientFactory) : IHttpP
 
     private async Task<DownloadResult> DownloadUrlAsync(Uri url, string destinationDir, CancellationToken ct)
     {
-        var client = httpClientFactory.GetHttpClient();
+        var client = httpClientFactory.CreateClient();
         var filePath = Path.Combine(destinationDir, FileNameSanitizer.Sanitize(url));
         var completed = false;
 
