0b111620e9e59b6af7f9b920f563e3fc2a3591ea
Lisanna <alisa020306@gmail.com>
2026-07-03 10:10:58 +0300

    push AnalyticsModels

--- 2026_MT_Gerashchenko_A_S_Lab_5/AnalyticsModels.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_5/AnalyticsModels.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _2026_MT_Gerashchenko_A_S_Lab_5.Models;
 
-namespace _2026_MT_Gerashchenko_A_S_Lab_5
-{
-    internal class AnalyticsModels
-    {
-    }
-}
+public record TopMethodEntry(string Test, long TimeMs, decimal Gain, string Processor);
+
+public record SpeedupEntry(string TestType, double AvgSpeedup, int Count);
+
+public record AnomalyEntry(string Test, long SingleMs, long MultiMs, long Overhead);
+
+public record EnvironmentEntry(string Processor, double AvgSingle, double AvgMulti, int Count);
+
+public record BestStructureEntry(string SizeGroup, decimal BestGain); namespace _2026_MT_Gerashchenko_A_S_Lab_5.Models;
+
+public record TopMethodEntry(string Test, long TimeMs, decimal Gain, string Processor);
+
+public record SpeedupEntry(string TestType, double AvgSpeedup, int Count);
+
+public record AnomalyEntry(string Test, long SingleMs, long MultiMs, long Overhead);
+
+public record EnvironmentEntry(string Processor, double AvgSingle, double AvgMulti, int Count);
+
+public record BestStructureEntry(string SizeGroup, decimal BestGain);
