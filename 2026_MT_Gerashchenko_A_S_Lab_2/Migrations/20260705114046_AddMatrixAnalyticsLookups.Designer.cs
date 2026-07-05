1af5909eff271ade720e9617880f628a5889728e
Lisanna <alisa020306@gmail.com>
2026-07-05 15:36:06 +0300

    push

--- 2026_MT_Gerashchenko_A_S_Lab_2/Migrations/20260705114046_AddMatrixAnalyticsLookups.Designer.cs
+++ 2026_MT_Gerashchenko_A_S_Lab_2/Migrations/20260705114046_AddMatrixAnalyticsLookups.Designer.cs
@@ -15,7 +15,7 @@ namespace _2026MTGerashchenkoASLab_2.Migrations
     partial class AddMatrixAnalyticsLookups
     {
         /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
